f6f6b7000691e01715df6c8b84031ba69054261a
rodrigo <rodrigobravo-@hotmail.com>
2019-08-26 21:26:53 -0300

    primer commit

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
+    //INCLUYENDO LIBRERIAS
+    include 'funciones.php';
+    //require 'funciones.php';
+
     echo "Hola Index";
+    /*Aplicación Nº 1 (Mostrar variables)
+        Realizar un programa que guarde su nombre en $nombre y su apellido en $apellido . Luego
+        mostrar el contenido de las variables con el siguiente formato: Pérez, Juan. 
+        Utilizar el operador de concatenación.*/
+        echo "<br> Ejercicio_01:";
+        $nombre="Juan";
+        $apellido="Perez";
+        echo  "<br>Nombre: ",$nombre, " Apellido: ",$apellido;
+        echo  "<br>".$nombre.", ".$apellido;
+        echo "<br><br><br> Funcion:";
+        saludar($nombre);
+
+        /*
+        Aplicación Nº 2 (Sumar dos números)
+        Hacer un programa en PHP que sume el contenido de dos variables $x = -3 y $y = 15. 
+        Mostrar el resultado final.
+        */ 
+
 ?>
